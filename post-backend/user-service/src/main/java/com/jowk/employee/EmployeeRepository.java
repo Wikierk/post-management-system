58d499a26353f1ddc2c6c64217d6103f6920f349
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2026-03-16 13:28:58 +0100

    feat: Define Employee and EmployeeRepository.

---
+++ post-backend/user-service/src/main/java/com/jowk/employee/EmployeeRepository.java
@@ -0,0 +1,23 @@
+package com.jowk.employee;
+
+import com.jowk.user.UserStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
+
+    Optional<Employee> findByUserIdAndUserStatus(UUID userId, UserStatus status);
+
+    @EntityGraph(attributePaths = {"user", "branch"})
+    Optional<Employee> findWithBranchAndUserByUserIdAndUserStatus(
+            UUID userId, UserStatus status);
+
+    Page<Employee> findByBranchId(UUID branchId, Pageable pageable);
+
+}
