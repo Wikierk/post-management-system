a3e8f48925cc7d6c462579e8c06f01f1fe078877
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2026-03-16 10:48:21 +0100

    feat: Define Report, ReportStatus and ReportRepository.

---
+++ post-backend/user-service/src/main/java/com/jowk/report/ReportRepository.java
@@ -0,0 +1,15 @@
+package com.jowk.report;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.UUID;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, UUID> {
+
+    Page<Report> findByAssignedAdminId(UUID assignedAdminId, Pageable pageable);
+    Page<Report> findByAuthorId(UUID authorId, Pageable pageable);
+
+}
---
+++ post-backend/user-service/src/main/java/com/jowk/report/ReportStatus.java
@@ -0,0 +1,8 @@
+package com.jowk.report;
+
+public enum ReportStatus {
+    WAITING,
+    IN_PROGRESS,
+    RESOLVED,
+    REJECTED
+}
